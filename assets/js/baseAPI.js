5d0b4be34ed15682ddf6f450cd0cb0742ccd1697
shiyinqing <435347316@qq.com>
2020-09-11 09:45:43 +0800

    完成主页的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,5 +2,19 @@ $.ajaxPrefilter(function(options) {
     //每次调用$.get() $.post() $.ajax()的时候会先调用ajaxPrefilter
     console.log(options.url);
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+        //统一为有权限的接口设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function() {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //强制清空token
+            localStorage.removeItem('token')
+                //强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 
 })
